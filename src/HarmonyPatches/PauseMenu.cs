2a6927ffafe09e0e54b59cd3f0dd1a34ec89e9e0
Clearwater <xxx@xxx.com>
2024-09-25 14:29:57 +0200

    Added confirmation for leaving bingo game when in-game.

--- src/HarmonyPatches/PauseMenu.cs
+++ src/HarmonyPatches/PauseMenu.cs
@@ -7,6 +7,33 @@ using static UltraBINGO.CommonFunctions;
 
 namespace UltraBINGO.HarmonyPatches;
 
+[HarmonyPatch(typeof(OptionsMenuToManager),"QuitMission")]
+public static class ConfirmLeaveGame
+{
+    [HarmonyPrefix]
+    public static bool confirmLeaveInGame(ref OptionsMenuToManager __instance)
+    {
+        if(GameManager.isInBingoLevel)
+        {
+            TextMeshProUGUI leaveText = GetGameObjectChild(GetGameObjectChild(__instance.quitDialog.gameObject,"Panel"),"Text (2)").GetComponent<TextMeshProUGUI>();
+            if(GameManager.playerIsHost())
+            {
+                leaveText.text = "<color=orange>WARNING</color>: Leaving now will <color=orange>end the game</color> for all players.\nAre you sure?";
+            }
+            else
+            {
+                leaveText.text = "Are you sure you want to leave?\n(<color=orange>WARNING</color>: You will not be able to rejoin.)";
+            }
+            __instance.quitDialog.ShowDialog();
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+}
+
 [HarmonyPatch(typeof(OptionsManager),"Start")]
 public static class PauseMenu
 {
@@ -15,7 +42,7 @@ public static class PauseMenu
     {
         if(GameManager.isInBingoLevel && getSceneName() != "Main Menu")
         {
-            GetGameObjectChild(GetGameObjectChild(__instance.pauseMenu,"Quit Mission"),"Text").GetComponent<TextMeshProUGUI>().text = "RETURN TO CARD";
+            GetGameObjectChild(GetGameObjectChild(__instance.pauseMenu,"Quit Mission"),"Text").GetComponent<TextMeshProUGUI>().text = "LEAVE GAME";
             BingoCardPauseMenu.Init(ref __instance);
             BingoCardPauseMenu.ShowBingoCardInPauseMenu(ref __instance);
         }
