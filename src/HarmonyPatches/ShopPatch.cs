8088717c2dacfd1b5aaf37653d39258513710816
Clearwater <xxx@xxx.com>
2024-10-02 14:51:50 +0200

    Added messages to terminal for unclaimed/claimed levels. Fixed tab stat display not working in Prime levels.

---
+++ src/HarmonyPatches/ShopPatch.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using HarmonyLib;
+using TMPro;
+using UltrakillBingoClient;
+using UnityEngine;
+
+using static UltraBINGO.CommonFunctions;
+
+namespace UltraBINGO.HarmonyPatches;
+
+[HarmonyPatch(typeof(ShopZone),"TurnOn")]
+public class ShopAddVRankInfo
+{
+    [HarmonyPostfix]
+    public static void AddVRankInfo(ShopZone __instance, Canvas ___shopCanvas)
+    {
+        if(GameManager.isInBingoLevel)
+        {
+            if(___shopCanvas != null)
+            {
+                TextMeshProUGUI origTip = getTextMeshProGUI(GetGameObjectChild(GetGameObjectChild(GetGameObjectChild(___shopCanvas.gameObject,"TipBox"),"Panel"),"TipText"));
+                
+                string coords = GameManager.currentRow + "-" + GameManager.currentColumn;
+                
+                string teamClaim = GameManager.CurrentGame.grid.levelTable[coords].claimedBy;
+                float time = GameManager.CurrentGame.grid.levelTable[coords].timeToBeat;
+                int style = GameManager.CurrentGame.grid.levelTable[coords].styleToBeat;
+                
+                float secs = time;
+                float mins = 0;
+                while (secs >= 60f)
+                {
+                    secs -= 60f;
+                    mins += 1f;
+                }
+                string formattedTime = mins + ":" + secs.ToString("00.000");
+                
+                string unclaimed = "This level is currently <color=orange>unclaimed</color>.\nHurry and be the first to <color=green>claim it for your team</color>!";
+                string claimedByOwnTeam = "This level is currently <color=green>claimed by your team</color>.\n<color=orange>Choose another level</color> to claim, or <color=orange>try and improve the current requirement</color> to make it harder for other teams to reclaim!";
+                string claimedByOtherTeam = "This level is currently claimed by the OTHER team.\n<color=orange>Beat</color> the current requirement to <color=green>reclaim it for your team</color>!";
+                
+                if(teamClaim == "NONE")
+                {
+                    origTip.text = unclaimed;
+                }
+                else
+                {
+                    origTip.text = "Claimed by: " + teamClaim + "\n\n" +
+                    (GameManager.CurrentGame.gameSettings.gameType == 0 ? "TIME" : "STYLE") + " TO BEAT: <color=orange>" + (GameManager.CurrentGame.gameSettings.gameType == 0 ? formattedTime : style) + "</color>\n\n" +
+                    (teamClaim == GameManager.currentTeam ? claimedByOwnTeam : claimedByOtherTeam);
+                }
+                
+                //Hide the CG and sandbox buttons
+                GameObject cgButton = GetGameObjectChild(GetGameObjectChild(___shopCanvas.gameObject,"Main Menu"),"CyberGrindButton");
+                cgButton.SetActive(false);
+                
+                GameObject sandboxButton = GetGameObjectChild(GetGameObjectChild(___shopCanvas.gameObject,"Main Menu"),"SandboxButton");
+                sandboxButton.SetActive(false);
+            }
+        }
+    }
+}
--- src/HarmonyPatches/StatWindow.cs
+++ src/HarmonyPatches/StatWindow.cs
@@ -4,28 +4,45 @@ using TMPro;
 using UltrakillBingoClient;
 using UnityEngine;
 
+using static UltraBINGO.CommonFunctions;
+
 namespace UltraBINGO.HarmonyPatches;
 
 [HarmonyPatch(typeof(LevelStats),"Start")]
 public class StatWindowStart
 {
+    public static GameObject originalChallengeText;
+    
     [HarmonyPostfix]
     public static void setupStatWindow(ref LevelStats __instance)
     {
         if(GameManager.isInBingoLevel)
         {
-            Logging.Message("In bingo level, swapping level stats panel content");
-            GameObject currentRequirementObject = __instance.time.transform.parent.gameObject;
-            currentRequirementObject.GetComponent<TextMeshProUGUI>().text = (GameManager.CurrentGame.gameSettings.gameType == 0 ? "TIME:" : "STYLE:");
             
             GameObject secrets = __instance.secrets[0].transform.parent.gameObject;
             secrets.SetActive(false);
             
-            GameObject challenge = __instance.challenge.transform.parent.gameObject;
-            challenge.GetComponent<TextMeshProUGUI>().text = "TO BEAT:";
+            if(getSceneName().Contains("P-"))
+            {
+                GameObject majorAssists = __instance.majorAssists.transform.parent.gameObject;
+                majorAssists.GetComponent<TextMeshProUGUI>().text = "TO BEAT:";
+                
+                originalChallengeText = GetGameObjectChild(__instance.gameObject,"Challenge Title");
+                originalChallengeText.GetComponent<TextMeshProUGUI>().text = "CLAIMED BY:";
+                originalChallengeText.SetActive(true);
+                
+                __instance.GetComponent<RectTransform>().sizeDelta = new Vector2(285f,285f);
+            }
+            else
+            {
+                GameObject challenge = __instance.challenge.transform.parent.gameObject;
+                challenge.GetComponent<TextMeshProUGUI>().text = "TO BEAT:";
             
-            GameObject majorAssists = __instance.majorAssists.transform.parent.gameObject;
-            majorAssists.GetComponent<TextMeshProUGUI>().text = "CLAIMED BY:";
+                GameObject majorAssists = __instance.majorAssists.transform.parent.gameObject;
+                majorAssists.GetComponent<TextMeshProUGUI>().text = "CLAIMED BY:";
+            }
+            
+
         }
     }
 }
@@ -33,6 +50,8 @@ public class StatWindowStart
 [HarmonyPatch(typeof(LevelStats),"CheckStats")]
 public class StatWindow
 {
+    public static GameObject originalChallengeText;
+    
     [HarmonyPostfix]
     public static void showRequirements(ref LevelStats __instance)
     {
@@ -50,14 +69,28 @@ public class StatWindow
             }
             
             string formattedTime = mins + ":" + secs.ToString("00.000");
-            
-            __instance.challenge.GetComponent<TextMeshProUGUI>().text = (GameManager.CurrentGame.gameSettings.gameType == 0 ?  (formattedTime) : GameManager.CurrentGame.grid.levelTable[coords].styleToBeat.ToString());
-            
+                        
             string colorTag = (currentTeamClaim != "NONE"
                 ? ("<color="+GameManager.CurrentGame.grid.levelTable[coords].claimedBy.ToLower()+">" + currentTeamClaim + "</color>")  : "NONE"); 
             
-            __instance.majorAssists.GetComponent<TextMeshProUGUI>().text = colorTag;
-            __instance.majorAssists.GetComponent<TextMeshProUGUI>().fontSize = 20;
+            //If we're in a prime level, use major assists text for to beat, and challenge text for claimed by.
+            if(getSceneName().Contains("P-"))
+            {
+                __instance.majorAssists.GetComponent<TextMeshProUGUI>().text = (GameManager.CurrentGame.gameSettings.gameType == 0 ?  (formattedTime) : GameManager.CurrentGame.grid.levelTable[coords].styleToBeat.ToString());
+
+                GetGameObjectChild(GetGameObjectChild(__instance.gameObject,"Challenge Title"),"Challenge").GetComponent<TextMeshProUGUI>().text = colorTag;
+                GetGameObjectChild(GetGameObjectChild(__instance.gameObject,"Challenge Title"),"Challenge").GetComponent<TextMeshProUGUI>().fontSize = 20;
+                
+            }
+            else
+            {
+                __instance.challenge.GetComponent<TextMeshProUGUI>().text = (GameManager.CurrentGame.gameSettings.gameType == 0 ?  (formattedTime) : GameManager.CurrentGame.grid.levelTable[coords].styleToBeat.ToString());
+                
+                __instance.majorAssists.GetComponent<TextMeshProUGUI>().text = colorTag;
+                __instance.majorAssists.GetComponent<TextMeshProUGUI>().fontSize = 20;
+            }
+
+
           
         }
     }
