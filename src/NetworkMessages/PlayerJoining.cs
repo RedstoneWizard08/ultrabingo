3949b3b94ae2b863b45573596ffec5d21d4717e4
Clearwater <neoware@laposte.net>
2025-04-13 13:18:18 +0200

    Fixed ranks of players joining not showing.

--- src/NetworkMessages/PlayerJoining.cs
+++ src/NetworkMessages/PlayerJoining.cs
@@ -6,6 +6,7 @@ public class PlayerJoiningMessage : MessageResponse
 {
     public string username;
     public string steamId;
+    public string rank;
 }
 
 public static class PlayerJoiningResponseHandler
@@ -15,6 +16,7 @@ public static class PlayerJoiningResponseHandler
         MonoSingleton<HudMessageReceiver>.Instance.SendHudMessage(response.username + " has joined the game.");
         Player newPlayer = new Player();
         newPlayer.username = response.username;
+        newPlayer.rank = response.rank;
         GameManager.CurrentGame.currentPlayers[response.steamId] = newPlayer;
         GameManager.RefreshPlayerList();
     }
